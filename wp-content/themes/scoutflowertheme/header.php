0c30f26e667830b0318ac6c9f57ced8999f36c11
Daniel Cambronero <cambroes@gmail.com>
2021-07-21 09:34:49 -0600

    edited frontpage content & header

--- wp-content/themes/scoutflowertheme/header.php
+++ wp-content/themes/scoutflowertheme/header.php
@@ -29,7 +29,7 @@
             <div class="container pt-3 pb-3">
                 <div class="row d-flex align-items-center justify-content-start">
                     <div class="col">
-                        <a class="" href="#"><h2>Flowex</h2></a>
+                        <a class="" href="#"><h2>Xflor</h2></a>
                     </div>
                     <div class="col-md-7 col-sm-12">
                         <?php if ( function_exists( 'aws_get_search_form' ) ) { aws_get_search_form(); } ?>
