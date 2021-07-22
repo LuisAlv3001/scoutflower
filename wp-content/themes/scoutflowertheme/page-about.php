e8edc2ce39c7a7b220662bf4f72e91d59fa44ba4
Daniel Cambronero <cambroes@gmail.com>
2021-07-22 16:08:00 -0600

    updated about us page

--- wp-content/themes/scoutflowertheme/page-about.php
+++ wp-content/themes/scoutflowertheme/page-about.php
@@ -4,14 +4,14 @@ get_header();?>
 
     <div class="container">
 
-        <div class="d-flex justify-content-center mb-3" style="font-size: 1.5rem;">
+        <!-- <div class="d-flex justify-content-center mb-3" style="font-size: 1.5rem;">
             <p class="col-8 text-center">Xflor is an independent clothing brand co-founded by a group of friends looking to make income to support their projects. In exchange of you supporting us monetarily you’ll get in return you clothing with not just badass designs but story and craft behind it.</p>
-        </div>
+        </div> -->
 
         <div class="row">
             <div class="col-8">
                 <h2 class="titles">Our story</h2>
-                <p>We are and always have been just a group of friends doing what groups of friends do but in the early months of 2021 two friends of the group had an idea; blending experiences and storytelling with artwork and creativity in order to make a wearable piece. Behind every design there’s a story, Xflor is the way to share our creations through clothing and generate and income for our audiovisual productions.</p>
+                <p>Xflor is founded by a friend crew called Exploradores Flor; we established the friend group and came up with the name back in 2015, when we were all still in high school. The years kept on going by and we remained friends, but on the early months of 2021, something switched on who we wanted to really be. It was then, that on an early morning in Pavas, San José; two original members came up with the idea of blending experiences and storytelling with artwork and creativity in order to create a wearable piece. That’s how Xflor clothing started, from a conversation between friends looking for a way to generate income to support their own personal projects. Now, a simple conversation about a fantasy is starting to become a reality, we are first launching our online store and we hope to reach out to as many people as possible; we want to make our customers a part of our story and share with them the experiences we collect along the way.</p>
 
             </div>
         </div>
@@ -23,16 +23,16 @@ get_header();?>
 
 
             <div class="col-6">
-                <h2 class="titles" >Our vision</h2>
-                <p>We want to provide a safe space through our community for anyone who wants a part of it and we want to make reach out to people in just as many ways as we can. We look forward for evolving into a platform for independent creators to share their work in the future.</p>
+                <h2 class="titles" >Our mission</h2>
+                <p>Our mission is to provide high quality clothing and exclusive designs while sharing our story and creations with the public.</p>
             </div>
         </div>
 
 
         <div class="row mt-2">
             <div class="col-6">
-                <h2 class="titles">Our mision</h2>
-                <p>We aim to not just sell a piece of clothing but to share a story through it; we don’t see it as a shirt but as a piece of our craft which design’s is a scene of a real-life situation. We want to make people a part of our story and share with them our creations along the way.</p>
+                <h2 class="titles">Our vision</h2>
+                <p>We will become the leading clothing brand in terms of creativity and originality, pushing ourselves to the limit to offer the public something they have never really seen before. </p>
             </div>
         <div class="col-6">
             <img src="<?php bloginfo('template_directory');?>/images/cancer.gif" width=300 alt="">
